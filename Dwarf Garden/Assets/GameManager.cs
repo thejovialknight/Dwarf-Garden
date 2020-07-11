eb055ec77e0575ad10d89717894d5c0d61b66c7d
thejovialknight <thejovialknight@gmail.com>
2020-07-10 20:58:05 -0700

    Initial Commit

---
+++ Dwarf Garden/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public string matchScene = "Match";
+    public string menuScene = "Menu";
+
+    public GameManager Instance;
+
+    void Awake() {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void EnterMatch() {
+        SceneManager.LoadScene(matchScene);
+    }
+
+    public void EnterMenu() {
+        SceneManager.LoadScene(menuScene);
+    }
+
+    public void CloseApplication() {
+        Application.Quit();
+    }
+}
